344ccc78e113a48c7e315404d7b7c5ba6c4fbb39
KvngDark <bastosg1701@gmail.com>
2024-10-05 22:19:39 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,31 @@
+
+// Esse código foi inspirado do site do EPA de alunos da 1110 de 2024
+let navBar = document.querySelector('#header')
+
+document.addEventListener("scroll", () => {
+    let scrollTop = window.scrollY
+
+    if(scrollTop > 0){
+        navBar.classList.add('rolar')
+    } else(
+        navBar.classList.remove('rolar')
+    )
+})
+
+VanillaTilt.init(document.querySelectorAll(".card"), {
+    max: 25,
+    speed: 400,
+    glare:true,
+    "max-glare":
+     0.5
+});
+
+gsap.fromTo(".logo-name" , {
+    y:50,
+    opacity:0,
+}, {
+    y:0,
+    opacity:1,
+    duration:2,
+    delay:0.5,
+})
